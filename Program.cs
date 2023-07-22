0bf2b5f10f1859ab4866622424e4900a44456153
wavy0 <smartmonkeypro@gmail.com>
2023-07-22 16:21:05 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace MultipleRoblox
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Starting multi-instance in 3 seconds.");
+            Thread.Sleep(3001);
+            new Mutex(true, "ROBLOX_singletonMutex");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Multi-instance started!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Do ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("NOT ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("close this window, otherwise all Roblox windows except one will close.");
+            Thread.Sleep(-1);
+        }
+    }
+}
